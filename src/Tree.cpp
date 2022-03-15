cbc3ede731c956486871cd76da1084c84ef294a7
Jdman6 <40027280+Jdman6@users.noreply.github.com>
2022-03-14 20:47:01 -0700

    Create Tree.cpp

---
+++ src/Tree.cpp
@@ -0,0 +1,22 @@
+/*Joshua Lindquist & Sarah Allen
+CS132 Winter 2022
+Final Project: Simulation
+
+Cpp file for the Tree class*/
+
+#include "Tree.h"
+
+
+Tree::Tree() {}
+
+string Tree::getType() {
+    return "Tree";
+}
+
+string Tree::toString() {
+    return "\xF0\x9F\x8C\xB2";
+}
+
+string Tree::getColor() {
+    return "yellow";
+}
