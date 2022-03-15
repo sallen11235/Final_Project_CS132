cdd366578e4901ba30a11ba7fb863fc77730c063
sallen11235 <101167120+sallen11235@users.noreply.github.com>
2022-03-14 22:47:52 -0700

    Time Log

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 CS132 Winter 2022
 Final Project: Simulation 
 
-Our project time log:
+Our project time log (Google Doc):
 https://docs.google.com/document/d/1KiuJqJVmSJtbU3N5LMyqXaZzCb5KfMjcwh2K-tIn_0Y/edit?usp=sharing
 
 For our final project we decided to simulate a village. The villagers consist of hunters and
